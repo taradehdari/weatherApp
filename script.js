e13a4ff0a5ed36117002f11531cec511143976f9
Tara Dehdari <tara.dehdari@gmail.com>
2020-08-17 12:18:51 -0700

    fixing errors

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-/*SEARCH BY USING A CITY NAME (e.g. athens) OR A COMMA-SEPARATED CITY NAME ALONG WITH THE COUNTRY CODE (e.g. athens,gr)*/
+
 const form = document.querySelector(".top-banner form");
 const input = document.querySelector(".top-banner input");
 const msg = document.querySelector(".top-banner .msg");
 const list = document.querySelector(".ajax-section .cities");
-/*PUT YOUR OWN KEY HERE - THIS MIGHT NOT WORK
-SUBSCRIBE HERE: https://home.openweathermap.org/users/sign_up*/
-const apiKey = "4d8fb5b93d4af21d66a2948710284366";
+
+const apiKey = "f5c8350d6a5e43ef91544fcc718ea89d";
 
 form.addEventListener("submit", e => {
   e.preventDefault();
